17c23b322ff51feaff8d1a52912ca33123c848cf
tmatkovic <toni.matkovic0@gmail.com>
2025-03-14 01:35:52 +0000

    Implemented horizontal Scrollbar

--- src/components/SearchExercises.tsx
+++ src/components/SearchExercises.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { Typography, Box, Button, Stack, TextField } from "@mui/material";
 import { fetchData, exerciseOptions, Exercise } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
   const [search, setSearch] = useState("");
   const [exercises, setExercises] = useState<Exercise[]>([]);
   const [bodyParts, setBodyParts] = useState<string[]>([]);
@@ -78,6 +79,9 @@ const SearchExercises = () => {
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", padding: "20px" }}>
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       </Box>
     </Stack>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,26 @@ import { Box } from "@mui/material";
 import Exercises from "../components/Exercises";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
+import { useState } from "react";
+import { Exercise } from "../utils/fetchData";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState<string>("all");
+  const [exercises, setExercises] = useState<Exercise[]>([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises  // TODO: CONVERT THIS INTO CONTEXT
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   );
 };
