e3b8a9fc38dfc7eb05b84a468dc13d4c9c9becdb
tmatkovic <toni.matkovic0@gmail.com>
2025-03-15 13:18:45 +0000

    Implemented custom horizontal scroll

--- src/components/HorizontalScrollbar.tsx
+++ src/components/HorizontalScrollbar.tsx
@@ -3,19 +3,29 @@ import BodyPart from "./BodyPart";
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>
+    <Box display="flex" alignItems="center">
+      <button onClick={() => document.getElementById('scrollable').scrollLeft -= 200}>{"<"}</button>
+      <Box
+      id="scrollable"
+      display="flex"
+      overflow="auto"
+      whiteSpace="nowrap"
+      width="100%"
+      >
       {data.map((item) => {
         return (
-          <Box key={item.id || item} title={item.id || item} m="0 40px">
-            <BodyPart
-              item={item}
-              bodyPart={bodyPart}
-              setBodyPart={setBodyPart}
-            />
-          </Box>
+        <Box key={item.id || item} title={item.id || item} m="0 40px">
+          <BodyPart
+          item={item}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+          />
+        </Box>
         );
       })}
-    </div>
+      </Box>
+      <button onClick={() => document.getElementById('scrollable').scrollLeft += 200}>{">"}</button>
+    </Box>
   );
 };
 
