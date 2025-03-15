0b203804fbce40195fd26c06cd261fa41034ece9
tmatkovic <toni.matkovic0@gmail.com>
2025-03-15 13:45:59 +0000

    Added nice arrows

--- src/components/HorizontalScrollbar.tsx
+++ src/components/HorizontalScrollbar.tsx
@@ -1,30 +1,47 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import BodyPart from "./BodyPart";
+import LeftArrow from "./../assets/icons/left-arrow.png";
+import RightArrow from "./../assets/icons/right-arrow.png";
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
     <Box display="flex" alignItems="center">
-      <button onClick={() => document.getElementById('scrollable').scrollLeft -= 200}>{"<"}</button>
-      <Box
-      id="scrollable"
-      display="flex"
-      overflow="auto"
-      whiteSpace="nowrap"
-      width="100%"
+      <Typography
+        sx={{ cursor: "pointer" }}
+        onClick={() =>
+          (document.getElementById("scrollable")!.scrollLeft -= 200)
+        }
       >
-      {data.map((item) => {
-        return (
-        <Box key={item.id || item} title={item.id || item} m="0 40px">
-          <BodyPart
-          item={item}
-          bodyPart={bodyPart}
-          setBodyPart={setBodyPart}
-          />
-        </Box>
-        );
-      })}
+        <img src={LeftArrow} alt="left" />
+      </Typography>
+      <Box
+        id="scrollable"
+        display="flex"
+        overflow="auto"
+        whiteSpace="nowrap"
+        width="100%"
+      >
+        {data.map((item) => {
+          return (
+            <Box key={item.id || item} title={item.id || item} m="0 40px">
+              <BodyPart
+                item={item}
+                bodyPart={bodyPart}
+                setBodyPart={setBodyPart}
+              />
+            </Box>
+          );
+        })}
       </Box>
-      <button onClick={() => document.getElementById('scrollable').scrollLeft += 200}>{">"}</button>
+      <Typography
+        sx={{ cursor: "pointer" }}
+        onClick={() =>
+          (document.getElementById("scrollable")!.scrollLeft += 200)
+        }
+      >
+        {" "}
+        <img src={RightArrow} alt="right" />
+      </Typography>
     </Box>
   );
 };
